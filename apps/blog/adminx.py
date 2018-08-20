b3a58d1686d2d404f6e68864496e74412be41a5e
强子 <hezq168@gmail.com>
2018-08-20 16:01:39 +0800

    del adminx.py

--- apps/blog/adminx.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2018/7/6 10:23
-# @Author  : Hezq
-# @Contact : Hezq168@gmail.com
-# @File    : adminx.py
-# @Project : mysite
-# @Software: PyCharm
